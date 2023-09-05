a9b9b3a6148afbfc064a556852cef33b9f41649c
proyecto <garymdfkinoak@gmail.com>
2023-09-04 21:46:08 -0500

    feat: Implemented app content

---
+++ week-04/tasky/src/App.vue
@@ -0,0 +1,34 @@
+<script>
+import {NewsApiService} from "@/news/service/new-api.service";
+import SideMenu from "@/news/components/side-menu.component.vue";
+import MainContent from "@/news/components/main-content.component.vue";
+import UnavailableContent from "@/news/components/unavailable-content.component.vue";
+import FooterContent from "@/news/components/footer-content.component.vue";
+
+export default {
+  name: 'App',
+  components: {FooterContent, UnavailableContent, MainContent, SideMenu},
+  data() {
+    return {
+      sidebarVisible: false,
+      articles: [],
+      errors: [],
+      newsApi: new NewsApiService()
+    }
+  },
+  created() {
+    this.getArticlesForSource('bbc-news');
+  },
+  methods: {
+
+  }
+}
+</script>
+
+<template>
+
+</template>
+
+<style scoped>
+
+</style>
---
+++ week-04/tasky/src/news/components/footer-content.component.vue
@@ -0,0 +1,20 @@
+<script setup>
+  export default {
+    name: "footer-content"
+  }
+</script>
+
+<template>
+<div class="grid bg-primary mt-4 p-4 align-content-start">
+  <div class="col-12 ml-3 align-items-center justify-content-center">
+    <p>Copyright &copy: 2023. ACME Studios</p>
+  </div>
+  <div class="col-12 ml-3 mt-3 align-items-center justify-content-center">
+    <p>Made with love by ACME Studios using Vue</p>
+  </div>
+</div>
+</template>
+
+<style scoped>
+
+</style>
