05f71f54b36b53501225113f0cfb139979437b67
proyecto <garymdfkinoak@gmail.com>
2023-09-04 21:23:22 -0500

    feat:  Implemented main content section

---
+++ src/news/components/main-content.component.vue
@@ -0,0 +1,21 @@
+<template>
+  <div v-for="article in articles">
+    <article-card :article="article"></article-card>
+  </div>
+</template>
+
+<script setup>
+import ArticleCard from "@/news/components/article-card.component.vue";
+
+export default {
+  name: "main-content",
+  components: {ArticleCard},
+  props: {
+    articles: Array
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
